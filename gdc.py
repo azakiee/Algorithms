10f5e2cc515298467f21ce68f1cc60e2de92076f
azakiee <148317913+azakiee@users.noreply.github.com>
2024-03-29 16:08:26 +0300

    Готовы три три рекурсии. Матрёшка умерла в зародыше...

---
+++ gdc.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+def gdc(a,b):
+    if a == b:
+        return a
+    elif a>b:
+        return gdc(a-b,b)
+    elif a<b:
+        return gdc(a,b-a)
+print(gdc(a,b))
---
+++ matreshka.py
@@ -0,0 +1,6 @@
+def matreha(n):
+    if n == 1:
+        print("Верх матрёшки 1")
+        print("Низ матрёшки 1")
+    else:
+        print
